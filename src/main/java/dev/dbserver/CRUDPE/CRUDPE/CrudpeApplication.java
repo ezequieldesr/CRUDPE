72ee4e00006da79e6fab1e9c7b5c4a1d2bb62052
EzequielSR <ezequieldesr@gmail.com>
2025-06-18 09:47:32 -0300

    first commit

---
+++ src/main/java/dev/dbserver/CRUDPE/CRUDPE/CrudpeApplication.java
@@ -0,0 +1,13 @@
+package dev.dbserver.CRUDPE.CRUDPE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudpeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudpeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/dbserver/CRUDPE/CRUDPE/CrudpeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dbserver.CRUDPE.CRUDPE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudpeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
