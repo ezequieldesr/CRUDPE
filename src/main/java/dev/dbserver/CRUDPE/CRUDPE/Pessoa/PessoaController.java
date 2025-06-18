8231ffe7ff2039de04f241aff46483ce49c1a7e2
EzequielSR <ezequieldesr@gmail.com>
2025-06-18 10:53:40 -0300

    feat: Add base code in PessoaController

--- src/main/java/dev/dbserver/CRUDPE/CRUDPE/Pessoa/PessoaController.java
+++ src/main/java/dev/dbserver/CRUDPE/CRUDPE/Pessoa/PessoaController.java
@@ -1,7 +1,28 @@
 package dev.dbserver.CRUDPE.CRUDPE.Pessoa;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/pessoas")
 public class PessoaController {
+
+    @GetMapping("/listar")
+    public String listarPessoas(){
+        return "Listar pessoas com sucesso";
+    }
+
+    @PostMapping("/criar")
+    public String criarPessoa(){
+        return "Pessoa criada com sucesso";
+    }
+
+    @PutMapping("/atualizar")
+    public String atualizarPessoa(){
+        return "Atualizar pessoa com sucesso";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarPessoa(){
+        return "Deletar pessoa com sucesso";
+    }
 }
