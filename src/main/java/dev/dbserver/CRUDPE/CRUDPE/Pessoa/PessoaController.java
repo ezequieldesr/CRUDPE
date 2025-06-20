298758dc6c801aec01498f83c418ebb66be4c310
EzequielSR <ezequieldesr@gmail.com>
2025-06-20 14:50:15 -0300

    feat: Add the ResponseEntity in PessoaController

--- src/main/java/dev/dbserver/CRUDPE/CRUDPE/Pessoa/PessoaController.java
+++ src/main/java/dev/dbserver/CRUDPE/CRUDPE/Pessoa/PessoaController.java
@@ -1,6 +1,8 @@
 package dev.dbserver.CRUDPE.CRUDPE.Pessoa;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,28 +14,60 @@ public class PessoaController {
     private PessoaService pessoaService;
 
     @GetMapping("/listar")
-    public List<PessoaDTO> listarPessoas(){
-        return pessoaService.listarPessoas();
+    public ResponseEntity<List<PessoaDTO>> listarPessoas(){
+        List<PessoaDTO> pessoa = pessoaService.listarPessoas();
+        return ResponseEntity.ok(pessoa);
     }
 
     @GetMapping("/{id}")
-    public PessoaDTO buscarPorId(@PathVariable Long id){ return pessoaService.buscarPessoaPorId(id);}
+    public ResponseEntity<?> buscarPorId(@PathVariable Long id){
+        PessoaDTO pessoa = pessoaService.buscarPessoaPorId(id);
+        if(pessoa != null){
+            return ResponseEntity.ok(pessoa);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("A pessoa com o id " + id + " nao existe nos nossos registros.");
+        }
+
+    }
 
     @PostMapping("/criar")
-    public PessoaDTO criarPessoa(@RequestBody PessoaDTO pessoaDTO){
-        return pessoaService.criarPessoa(pessoaDTO);
+    public ResponseEntity<String> criarPessoa(@RequestBody PessoaDTO pessoa){
+        PessoaDTO pessoaDTO = pessoaService.criarPessoa(pessoa);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Pessoa criada com sucesso: " + pessoaDTO.getNome() + " (ID): " + pessoaDTO.getId());
     }
 
     @PutMapping("/atualizar/{id}")
-    public PessoaDTO atualizarPessoa(@PathVariable Long id, @RequestBody PessoaDTO pessoaAtualizada){
-        return pessoaService.atualizarPessoa(id,pessoaAtualizada);
+    public ResponseEntity<?> atualizarPessoa(@PathVariable Long id, @RequestBody PessoaDTO pessoaAtualizada){
+        PessoaDTO pessoa = pessoaService.atualizarPessoa(id,pessoaAtualizada);
+        if(pessoa != null){
+            return ResponseEntity.ok(pessoa);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Pessoa com o id: " + id + " nao existe nos nossos registros");
+        }
     }
 
     @DeleteMapping("/deletar/{id}")
-    public void deletarPessoa(@PathVariable Long id){
-         pessoaService.deletarPessoaPorId(id);
+    public ResponseEntity<String> deletarPessoa(@PathVariable Long id){
+         if(pessoaService.buscarPessoaPorId(id) != null){
+             pessoaService.deletarPessoaPorId(id);
+             return ResponseEntity.ok("Pessoa com o ID " + id + " deletado com sucesso!");
+         } else{
+             return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                     .body("A pessoa com o id " + id + " nao existe nos nossos registros.");
+         }
     }
 
     @GetMapping("/idade/{id}")
-    public int mostrarIdade(@PathVariable Long id){return pessoaService.mostrarIdade(id);}
+    public ResponseEntity<String> mostrarIdade(@PathVariable Long id){
+        PessoaDTO pessoa = pessoaService.buscarPessoaPorId(id);
+        if(pessoa != null){
+            return ResponseEntity.ok("A pessoa tem " + pessoaService.mostrarIdade(id));
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("A pessoa com o id " + id + " nao existe nos nossos registros.");
+        }
+    }
 }
