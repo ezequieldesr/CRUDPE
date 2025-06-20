c54567e750f60dcbe040b3adc1f2be04d42a4e7e
EzequielSR <ezequieldesr@gmail.com>
2025-06-20 16:04:25 -0300

    feat: Add the functions create, findById and list enderecos in EnderecoService for EnderecoController

--- src/main/java/dev/dbserver/CRUDPE/CRUDPE/Endereco/EnderecoService.java
+++ src/main/java/dev/dbserver/CRUDPE/CRUDPE/Endereco/EnderecoService.java
@@ -1,7 +1,36 @@
 package dev.dbserver.CRUDPE.CRUDPE.Endereco;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class EnderecoService {
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+
+    @Autowired
+    private EnderecoMapper enderecoMapper;
+
+    public List<EnderecoDTO> listarEnderecos(){
+        List<EnderecoModel> enderecos = enderecoRepository.findAll();
+        return enderecos.stream()
+                .map(enderecoMapper::map)
+                .collect(Collectors.toList());
+    }
+
+    public EnderecoDTO buscarEnderecoPorId(Long id){
+        Optional<EnderecoModel> enderecoModel = enderecoRepository.findById(id);
+        return enderecoModel.map(enderecoMapper::map).orElse(null);
+    }
+
+    public EnderecoDTO criarEndereco(EnderecoDTO enderecoDTO){
+        EnderecoModel endereco = enderecoMapper.map(enderecoDTO);
+        endereco = enderecoRepository.save(endereco);
+        return enderecoMapper.map(endereco);
+    }
+
 }
